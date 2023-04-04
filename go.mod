466636da7862ef39c7568c037449cb3cb37c271a
Krystian Lewandowski <k.lewandowski@global4net.com>
2023-04-04 16:41:41 +0200

    Add hashed password

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/golang-jwt/jwt/v5 v5.0.0-rc.2 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 	gorm.io/driver/sqlite v1.4.4 // indirect
 	gorm.io/gorm v1.24.6 // indirect
 )
--- seciurity/passwords.go
+++ seciurity/passwords.go
@@ -1,1 +1,20 @@
 package helper
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+func ComaparePasswords(hashedPassword string, password string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
