992015582df07c9c0d544530fa6f074ef98c3150
Krystian Lewandowski <k.lewandowski@global4net.com>
2023-04-04 16:52:39 +0200

    chanche arg on dCrypt const

--- seciurity/passwords.go
+++ seciurity/passwords.go
@@ -3,7 +3,7 @@ package helper
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
