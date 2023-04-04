705d748d296239654c1fbe2bcbf4f76899a54422
Krystian Lewandowski <k.lewandowski@global4net.com>
2023-04-04 15:54:39 +0200

    validate and parseUser

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"edu/seciurity"
 	"fmt"
 
+	"log"
+
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"log"
 )
 
 func main() {
@@ -37,13 +39,23 @@ func main() {
 	// }
 	// fmt.Println(user)
 
-	token, err := helper.CreateJWTToken(uint(user.ID))
+	token, err := helper.CreateJWTToken(uint(user.ID), "secretToken")
 	if err != nil {
 		fmt.Println("KUPA")
 		log.Fatal(err)
 	}
 
 	fmt.Println(token)
+
+	parsedToken, err := helper.ValidateJWTToken(token, "secretToken")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parsedUser := parsedToken.Claims.(jwt.MapClaims)
+
+	fmt.Println(parsedUser)
+
 }
 
 func addUser() user.User {
--- seciurity/jwt.go
+++ seciurity/jwt.go
@@ -2,11 +2,11 @@ package helper
 
 import "github.com/golang-jwt/jwt/v5"
 
-func CreateJWTToken(userID uint) (string, error) {
+func CreateJWTToken(userID uint, secret string) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["user_id"] = userID
-	Ptoken, err := token.SignedString([]byte("elo"))
+	Ptoken, err := token.SignedString([]byte(secret))
 	if err != nil {
 		return "", err
 	}
